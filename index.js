d653a1aa36c42dcbf36adf9b2397df0a1ea526d3
beatrice <beatrice.kamoni@student.moringaschool.com>
2026-06-25 21:39:18 +0300

    function createLoginTracker

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
+function createLoginTracker (userInfo){
+  const LoginInfo = {
+    Username: "user1",
+    Password: "password123"
+  };
+  let attemptCount = 3
+}
+const checkPassword = (PasswordAttempt) => {
+  attemptCount++;
+  if(attemptCount <=3){
+    userInfo.Password = "password123"
+    console.log("Login successful");
+  }else {
+    console.log("Login failed");
+  }
+  if(attemptCount >3){
+    console.log("Account locked due too many failed login attempts");
+  }
+    
+};
+checkPassword("abc")
 
 
 
